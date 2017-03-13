946c6208fc5c897738ca93e1aa00c5c46f0e334c
John Perrin <jperrin@travelrepublic.co.uk>
2017-03-13 08:26:21 +0000

    start of day 08

---
+++ day08/day08_test.go
@@ -0,0 +1,20 @@
+package day08
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func Test_Regex(t *testing.T) {
+	Convey("Instruction Regex", t, func() {
+		Convey("Should detect rect instructions", func() {
+			instruction, error := parseInstruction("rect 2x2")
+
+			So(error, ShouldEqual, nil)
+			So(instruction.instuction, ShouldEqual, "")
+			So(instruction.reg1, ShouldEqual, 2)
+			So(instruction.reg2, ShouldEqual, 2)
+		})
+	})
+}

c24bb98e5e945c773f41ba2e606b6b14ec6b51bd
John Perrin <jperrin@travelrepublic.co.uk>
2017-04-10 17:07:52 +0100

    changes!

--- day17/day17.go
+++ day17/day17.go
@@ -1,5 +1,18 @@
 package day17
 
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// starts at zero
+var gridSize = 3
+
+type point struct {
+	x int
+	y int
+}
+
 // PartOne solves day 17 part one.
 func PartOne(input string) (string, error) {
 	return "", nil
@@ -8,4 +21,26 @@ func PartOne(input string) (string, error) {
 // PartTwo solves day 17 part two.
 func PartTwo(input string) (string, error) {
 	return "", nil
+}
+
+func getHash(input string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(input))
+	hash := hex.EncodeToString(hasher.Sum(nil))
+	return hash
+}
+
+func isOpen(input rune) bool {
+	return input >= 98 && input <= 102
+}
+
+func getDoors(input string, pos point) []bool {
+	hash := []rune(getHash(input)[:4])
+
+	up := pos.x != 0 && isOpen(hash[:1][0])
+	down := pos.x != gridSize && isOpen(hash[1:2][0])
+	left := pos.y != 0 && isOpen(hash[2:3][0])
+	right := pos.y != gridSize && isOpen(hash[3:4][0])
+
+	return []bool{up, down, left, right}
 }
--- day17/day17_test.go
+++ day17/day17_test.go
@@ -1,2 +1,27 @@
 package day17
 
+import "testing"
+import . "github.com/smartystreets/goconvey/convey"
+import "log"
+
+func Test_Day17(t *testing.T) {
+	Convey("-> Should be able to do some test stuff", t, func() {
+		log.Println(getHash(testInput()))
+
+		log.Println(getDoors(PuzzleInput(), point{0, 0}))
+	})
+
+	Convey("-> Should be able to tell if a door is open or not", t, func() {
+		So(isOpen('a'), ShouldBeFalse)
+		So(isOpen('b'), ShouldBeTrue)
+		So(isOpen('c'), ShouldBeTrue)
+		So(isOpen('d'), ShouldBeTrue)
+		So(isOpen('e'), ShouldBeTrue)
+		So(isOpen('f'), ShouldBeTrue)
+		So(isOpen('g'), ShouldBeFalse)
+		So(isOpen('h'), ShouldBeFalse)
+		So(isOpen('i'), ShouldBeFalse)
+		So(isOpen('j'), ShouldBeFalse)
+		So(isOpen('k'), ShouldBeFalse)
+	})
+}

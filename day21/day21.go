cbe36bde7e541bd2d99e4d1b07bd46d9e8424fbc
John Perrin <jperrin@travelrepublic.co.uk>
2017-05-16 08:50:26 +0100

    some day 21 stuff

--- day21/day21.go
+++ day21/day21.go
@@ -1,5 +1,38 @@
 package day21
 
+import "strings"
+
+type password []rune
+
+func getRotatedPos(length int, shift int, position int) int {
+	actualShift := shift % length
+	return (position + actualShift) % length
+}
+
+func (p password) toString() string {
+	return string(p)
+}
+
+func (p password) rotate(x int) {
+	temp := make(password, len(p))
+	copy(temp, p)
+	for index := range p {
+		p[index] = temp[getRotatedPos(len(p), x, index)]
+	}
+}
+
+func (p password) swapPos(x int, y int) {
+	temp := p[x]
+	p[x] = p[y]
+	p[y] = temp
+}
+
+func (p password) swapChar(x rune, y rune) {
+	posX := strings.IndexRune(string(p), rune(x))
+	posY := strings.IndexRune(string(p), rune(y))
+	p.swapPos(posX, posY)
+}
+
 // PartOne solves day 21 part one.
 func PartOne(input string) (string, error) {
 	return "", nil
--- day21/day21_test.go
+++ day21/day21_test.go
@@ -3,10 +3,80 @@ package day21
 import (
 	"testing"
 
-	"github.com/smartystreets/goconvey/convey"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
-func Test_Day21(t *testing.T) {
-	convey.Convey("Day 21 should be able to", t, func() {
+func Test_Day21_getRotatedPos(t *testing.T) {
+	Convey("Get determine where to get a rotated char from", t, func() {
+		So(getRotatedPos(3, 10, 1), ShouldEqual, 2)
+
+		So(getRotatedPos(3, 1, 0), ShouldEqual, 1)
+		So(getRotatedPos(3, 1, 1), ShouldEqual, 2)
+		So(getRotatedPos(3, 1, 2), ShouldEqual, 0)
+
+		So(getRotatedPos(5, 10, 1), ShouldEqual, 1)
+		So(getRotatedPos(5, 11, 1), ShouldEqual, 2)
+		So(getRotatedPos(5, 12, 1), ShouldEqual, 3)
+		So(getRotatedPos(5, 13, 1), ShouldEqual, 4)
+		So(getRotatedPos(5, 14, 1), ShouldEqual, 0)
+		So(getRotatedPos(5, 15, 1), ShouldEqual, 1)
+	})
+}
+
+func Test_Day21_rotateLeft(t *testing.T) {
+	Convey("Should be able to rotate a string by a specified shift", t, func() {
+		var pass password = []rune("wer")
+		pass.rotate(1)
+		So(pass.toString(), ShouldEqual, "erw")
+		pass.rotate(1)
+		So(pass.toString(), ShouldEqual, "rwe")
+		pass.rotate(1)
+		So(pass.toString(), ShouldEqual, "wer")
+	})
+}
+
+func Test_Day21_rotateRight(t *testing.T) {
+	Convey("Should be able to rotate a string by a specified shift", t, func() {
+		var pass password = []rune("wer")
+		pass.rotate(-1)
+		So(pass.toString(), ShouldEqual, "rew")
+		pass.rotate(-1)
+		So(pass.toString(), ShouldEqual, "wre")
+		pass.rotate(-1)
+		So(pass.toString(), ShouldEqual, "ewr")
+	})
+}
+func Test_Day21_b(t *testing.T) {
+	Convey("Day 21 should be able to", t, func() {
+		Convey("Get determine where to get a rotated char from", func() {
+			So(getRotatedPos(3, 10, 1), ShouldEqual, 2)
+
+			So(getRotatedPos(3, 1, 0), ShouldEqual, 1)
+			So(getRotatedPos(3, 1, 1), ShouldEqual, 2)
+			So(getRotatedPos(3, 1, 2), ShouldEqual, 0)
+
+			So(getRotatedPos(5, 10, 1), ShouldEqual, 1)
+			So(getRotatedPos(5, 11, 1), ShouldEqual, 2)
+			So(getRotatedPos(5, 12, 1), ShouldEqual, 3)
+			So(getRotatedPos(5, 13, 1), ShouldEqual, 4)
+			So(getRotatedPos(5, 14, 1), ShouldEqual, 0)
+			So(getRotatedPos(5, 15, 1), ShouldEqual, 1)
+		})
+
+		Convey("Swap character positions in a string", func() {
+			var pass password = []rune("wer")
+			pass.swapPos(1, 2)
+			So(string(pass), ShouldEqual, "wre")
+			pass.swapPos(0, 1)
+			So(string(pass), ShouldEqual, "rwe")
+		})
+
+		Convey("Swap characters in a string", func() {
+			var pass password = []rune("wer")
+			pass.swapChar(rune('r'), rune('e'))
+			So(string(pass), ShouldEqual, "wre")
+			pass.swapChar(rune('e'), rune('w'))
+			So(string(pass), ShouldEqual, "erw")
+		})
 	})
 }
